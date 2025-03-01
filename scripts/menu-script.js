5b16d13d86397b0ad55d1ab60e0ad00ce18102a8
Herdeir0 <david.herdeiro@gmail.com>
2025-03-01 19:07:49 +0000

    New mobile menu

--- scripts/menu-script.js
+++ scripts/menu-script.js
@@ -1,15 +1,13 @@
+const elementBody = document.querySelector('html'); 
+
 function toggleDisplay() {
     var element = document.getElementById("menu");
-    if (element.style.top === "-100%") {
-        element.style.top = "0%";
-        element.style.maxWidth = "100%";
-        element.style.padding = "32px 16px 32px 16px";
-        element.style.position = "relative";
+    if (element.style.display === "none") {
+        element.style.display = "flex";
+        elementBody.style.overflow = "hidden";
     } else {
-        element.style.maxWidth = "100%";
-        element.style.top = "-100%";
-        element.style.padding = "0em 0em 0em 0em";
-        element.style.position = "absolute";
+        element.style.display = "none";
+        elementBody.style.overflow = "auto";
     }
 
     var element = document.getElementById("line1");
@@ -30,19 +28,5 @@ function toggleDisplay() {
     } else {
         element.style.display = "block";
         element.style.rotate = "0deg";
-    }
-    
-    var element = document.getElementById("toggleButton");
-    if (element.style.right === "17px") {
-        element.style.right = "10px";
-    } else {
-        element.style.right = "17px";
-    }
-
-    var element = document.getElementById("list");
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
     }
 }
